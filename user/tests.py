87b9a735221ad9109271a3f7789c80ec22949709
Farzam <farzamgt@gmail.com>
2025-09-26 09:02:58 +0300

    refactor(tests): use reverse in user tests instead of hardcoded URLs

--- user/tests.py
+++ user/tests.py
@@ -2,10 +2,12 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.urls import reverse
 
-REGISTER_URL = "/api/user/register/"
-LOGIN_URL = "/api/user/login/"
-ME_URL = "/api/user/me/"
+
+REGISTER_URL = reverse("user:create")
+LOGIN_URL = reverse("user:login")
+ME_URL = reverse("user:manage")
 
 
 class UserApiTests(TestCase):
